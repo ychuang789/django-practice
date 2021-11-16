d54cc597a29731072b57af3fc8cde87bb1573f4a
Weber <ychuang@eland.com.tw>
2021-11-16 09:10:42 +0800

    add the execution mode(dev or prod)

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -81,26 +81,31 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+if bool(config['ExecuteMode']['PRODUCTION']):
+    db_info = config['DatabaseInfo']
 
-
-db_info = config['DatabaseInfo']
-
-DATABASES = {
-    'default': {
-        'ENGINE': db_info['ENGINE'],
-        'NAME': db_info['NAME'],
-        'USER': db_info['USER'],
-        'PASSWORD': db_info['PASSWORD'],
-        'HOST': db_info['HOST'],
-        'PORT': db_info['PORT'],
+    DATABASES = {
+        'default': {
+            'ENGINE': db_info['ENGINE'],
+            'NAME': db_info['NAME'],
+            'USER': db_info['USER'],
+            'PASSWORD': db_info['PASSWORD'],
+            'HOST': db_info['HOST'],
+            'PORT': db_info['PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+
+
+
+
 
 
 # Password validation
